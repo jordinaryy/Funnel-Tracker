8636d2056831392b7f05c047b56941ec6185cb81
jordinaryy <jordankrueger25@gmail.com>
2026-05-14 02:03:21 -0400

    backend is connected to database successfully

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 
+const pool = require('./db')
 const app = express()
 const PORT = process.env.PORT || 5000
 
@@ -11,6 +12,15 @@ app.use(express.json())
 //test route
 app.get('/', (req, res) => {
   res.json({ message: 'Funnel Tracker is running!'})
+})
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()')
+    res.json({ message : 'Database connected', time: result.rows[0].now })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
 })
 
 app.listen(PORT, () => {
